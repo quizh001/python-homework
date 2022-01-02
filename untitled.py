830613144983aa9acc3241aa5291eff56cd9ba67
quizh001 <quizh001@umn.edu>
2022-01-02 16:27:11 -0600

    casi casi

--- untitled.py
+++ untitled.py
@@ -1,5 +1,19 @@
-# @TODO: From the pathlib library, import the main class Path
-
-# From the pathlib library, import the main class Path
+# @TODO: Import and csv library
 from pathlib import Path
-csv
+import csv
+
+# @TODO: Check the current directory where the Python program is executing from
+
+# Check the current directory where the Python program is executing from
+print(f"Current Working Directory: {Path.cwd()}")
+# set the file path
+csvpath = Path('budget_data.csv')
+# @TODO: Set file paths for menu_data.csv and sales_data.csv
+menu_filepath = Path('')
+sales_filepath = Path('')
+
+# @TODO: Initialize list objects to hold our menu and sales data
+menu = []
+sales = []
+
+# @TODO: Read in the menu data into the menu list
